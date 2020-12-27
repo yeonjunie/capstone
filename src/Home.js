c8cbcc37e87f9e25e29ce6811b52537b38f0c0ab
Yeon Jun Kim <yeon_jun_kim@brown.edu>
2020-12-27 23:09:13 +0900

    login and home

--- src/Home.js
+++ src/Home.js
@@ -16,8 +16,21 @@ class Home extends React.Component {
   }
   render() {
     return (
-      <Header goToHome={this.goHome} goToLogin={this.goLogin} goToSignUp={this.goSignUp} page={"home"}/>
-    );
+      <div className="home">
+        <Header goToHome={this.goHome} goToLogin={this.goLogin} goToSignUp={this.goSignUp} page={"home"}/>
+        <div className="welcome">welcome</div>
+        <div className="welcome-desc">Join our network of electricians, mechanics and much more</div>
+        <div className="survey-desc">Fill out a <b>3 minute survey</b> to find out where you’re needed!</div>
+        <div className="registration-component">
+        <div className="registration-button">        
+          <div className="registration-desc">click here to get started</div>
+        </div>
+      </div>
+      <div className="background-img-container">
+        <img src="https://i.ibb.co/Jj5N77W/Screen-Shot-2020-12-27-at-17-23-04.png"/>
+      </div>
+      </div>
+      );
   }
 }
 
--- src/Login.js
+++ src/Login.js
@@ -1,10 +1,43 @@
 import React from 'react';
 import './Login.css';
+import Header from "./Header.js";
+import TextField from '@material-ui/core/TextField';
+// import { makeStyles } from '@material-ui/core/styles';
+
 
 class Login extends React.Component {
+  goHome = () => {
+    this.props.history.push("/");
+  }
+
+  goLogin = () => {
+    this.props.history.push("/login");
+  }
+
+  goSignUp = () => {
+    this.props.history.push("/registration");
+  }
   render() {
     return(
-      <div></div>
+      <div className="login">
+        <Header goToHome={this.goHome} goToLogin={this.goLogin} goToSignUp={this.goSignUp} page={"home"}/>
+        <div className="white">
+          <div className="welcomeback">
+            welcome back
+          </div>
+          <form className="login-form" noValidate autoComplete="off">
+            <div>
+              <TextField id="standard-required" label="username" />
+            </div>
+            <div className="password">
+              <TextField id="standard-password-input" type="password" label="password" />
+            </div>
+        </form>
+        <div className="login-button-container">
+          <div className="login-button">login</div>
+        </div>
+        </div>
+      </div>
     );
   }
 }
