ae617f031402bee1b06a3cb652c17aaf41f69767
Snigdha Sinha <snigdhasinha@Snigdhas-MBP.fios-router.home>
2021-01-07 13:12:36 -0500

    jobboard responsive

--- src/JobBoard.js
+++ src/JobBoard.js
@@ -31,31 +31,44 @@ class JobBoard extends React.Component {
 
     return (
       <div className="jobboard">
+        <div className="header-div">
         <Header goToHome={this.goHome} goToLogin={this.goLogin} goToSignUp={this.goSignUp} page={"home"}/>
-        <div className="sidebar">
-            
-            {/* OnClick take user back to form editing */}
-                <div className="edit">edit</div> 
-
-                <div className="profile-pic"></div>
-                <div className="my-profile-header">My Profile</div>
-                <div className="expertise">Expertise: Electronics</div>
-                <div className="location">{"Location: " + data[1]}</div>
-                
-                <div className="yoe">{"Years of Experience: " + data[2]}</div>
-                
-                <div className="min-wage">{"Minimum Wage: " + data[3]}</div>
-                
-                <div className="license">{"License: " + data[4]}</div>
-              
         </div>
         <div className="main-area">
-
-            <div className="recommended-jobs-header">Recommended Jobs</div>
-            <JobCard curr_data={data} company={companies[0]}/>
-            <JobCard curr_data={data} company={companies[1]}/>
-            <JobCard curr_data={data} company={companies[2]}/>
-            </div>
+            <div className="sidebar">
+                
+                {/* OnClick take user back to form editing */}
+                <div className="edit-row">
+                    <div className="edit">edit</div> 
+                </div>
+                  <div className="profile-row">
+                    <div className="profile-pic"></div>
+                    <div className="my-profile-header">My Profile</div>
+                  </div>
+                  <div className="preferences-div">
+                    <div className="expertise">Expertise: Electronics</div>
+                    <div className="location">{"Location: " + data[1]}</div>
+                    
+                    <div className="yoe">{"Years of Experience: " + data[2]}</div>
+                    
+                    <div className="min-wage">{"Minimum Wage: " + data[3]}</div>
+                    
+                    <div className="license">{"License: " + data[4]}</div>
+                    </div>
+                  
+                </div>
+                <div className="job-area">
+                <div className="recommended-div">
+                <div className="recommended-jobs-header">Recommended Jobs</div>
+                </div>
+                <div className="jobs-div">
+                <JobCard curr_data={data} company={companies[0]}/>
+                <JobCard curr_data={data} company={companies[1]}/>
+                <JobCard curr_data={data} company={companies[2]}/>
+                </div>
+                
+                </div>
+          </div>
         
       </div>
       );
